e15790bb2dbb183211f6f94ae04965515449fa58
SebCoffee <sebastien.noel.pro@gmail.com>
2018-04-04 15:04:43 +0200

    seance 1

--- DotNet_1/Program.cs
+++ DotNet_1/Program.cs
@@ -4,12 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ liens vers le tutoriel : 
+
+    https://docs.microsoft.com/en-us/aspnet/mvc/overview/getting-started/getting-started-with-ef-using-mvc/creating-an-entity-framework-data-model-for-an-asp-net-mvc-application
+     
+     */
+
 namespace DotNet_1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("hello");
+            SchoolContext db = new SchoolContext();
+            Console.WriteLine("Context ok");
+            foreach (Student s in db.Students)
+            {
+                Console.WriteLine(s.LastName);
+            }
+
+            Console.Read();
         }
     }
 }
